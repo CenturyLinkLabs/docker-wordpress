a03fc3958b8897c9d2da95faa71fd2571fae3e45
patocox <patrickocox@gmail.com>
2014-07-28 13:43:58 -0500

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+if [ -f /.mysql_db_created ]; 
+then
+        exec supervisord -n
+        exit 1
+fi
+
+sleep 5
+DB_EXISTS=$(mysql -uroot -p$DB_PASSWORD -h$DB_1_PORT_3306_TCP_ADDR -P$DB_1_PORT_3306_TCP_PORT -e "SHOW DATABASES LIKE 'wordpress';" | grep "wordpress" > /dev/null; echo "$?")
+
+if [[ DB_EXISTS -eq 1 ]]; 
+then
+        echo "=> Creating database wordpress"
+        RET=1
+        while [[ RET -ne 0 ]]; do
+                sleep 5
+                mysql -uroot -p$DB_PASSWORD -h$DB_1_PORT_3306_TCP_ADDR -P$DB_1_PORT_3306_TCP_PORT -e "CREATE DATABASE wordpress"
+                RET=$?
+        done
+        echo "=> Done!"
+else
+        echo "=> Skipped creation of database wordpress – it already exists."
+fi
+
+touch /.mysql_db_created
+exec supervisord -n
